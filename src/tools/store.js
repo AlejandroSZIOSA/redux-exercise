5acf82fe9df85b617d57729224d3a503d2151f4b
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-09-16 13:10:39 +0200

    Cleaning the code

--- src/tools/store.js
+++ src/tools/store.js
@@ -39,51 +39,6 @@ const productsSlice = createSlice({
   },
 });
 
-/* const productsReducer = (
-  state = {
-    productList: initialValuesProducts,
-    cartList: [],
-    totalSuma: 0,
-  },
-  action
-) => {
-  if (action.type === "ADD_PRODUCT") {
-    return {
-      ...state, // Copy existing state
-      cartList: [...state.cartList, action.product], // Create a new array with the new product
-    };
-  }
-
-  if (action.type === "REMOVE_PRODUCT") {
-    return {
-      ...state, // Copy the existing state
-      cartList: state.cartList.filter((product) => product.id !== action.id),
-    };
-  }
-
-  if (action.type === "TOTAL_SUMA") {
-    return {
-      productList: state.productList, //holds the values of the Product List
-      cartList: state.cartList, //holds the values of the cart
-      totalSuma: action.suma,
-    };
-  }
-
-  if (action.type === "CHANGE_QUANTITY") {
-    return {
-      ...state, // Copy existing state
-      cartList: state.cartList.map(
-        (p) =>
-          p.id === action.payload.id // Check if the product matches the given id
-            ? { ...p, ...(p.quantity = action.payload.newQuantity) } // Update the product with new data
-            : p // Keep the other products unchanged
-      ),
-    };
-  }
-
-  return state;
-}; */
-
 const store = configureStore({ reducer: productsSlice.reducer });
 
 export const productsSliceActions = productsSlice.actions;
